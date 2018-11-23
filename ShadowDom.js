4c4131aa1ee4c157b943b3b8f291a686692e41e4
dneilson <dneilson@sofi.org>
2018-11-22 17:24:06 -0700

    Update ShadowDom.js

--- ShadowDom.js
+++ ShadowDom.js
@@ -2,8 +2,8 @@
  $.fn.ShadowNode = function () {
    var id = $(this).prop('id');
    if ( $('body').children().is('#'+id) === false ) {
-      $(this).css('display','none')
-      this["0"].attachShadow({mode:'open'});
+    $(this).css('display','none')
+    this["0"].attachShadow({mode:'open'});
     $(this).appendTo('body')
     $($(this).prop('shadowRoot')).append('<div/>')
     $($(this).prop('shadowRoot')).wrapInner('<body/>')
@@ -18,19 +18,19 @@
     $($(this).prop('shadowRoot')).wrapInner('<body/>')
     $($(this).prop('shadowRoot')).wrapInner('<html/>')
     $($(this).prop('shadowRoot')).find('html').prop('innerHTML', '<head></head><body></body>');
-     }
-     $(this).fadeIn()
-     return this
+   }
+   $(this).fadeIn()
+   return this
  }
  $.fn.ShadowBody = function (NewContent) {
   $($(this).prop('shadowRoot')).find('body').prop('innerHTML',NewContent)
-   return this
+  return this
  }
  $.fn.ShadowHead = function (NewContent) {
   $($(this).prop('shadowRoot')).find('head').prop('innerHTML',NewContent)
-   return this
+  return this
  }
  $.fn.ShadowFind = function (Target) {
-   return $($(this).prop('shadowRoot')).find(Target)
+  return $($(this).prop('shadowRoot')).find(Target)
  }
  })(jQuery)
